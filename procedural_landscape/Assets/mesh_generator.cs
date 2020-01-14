cd1d46a309942faf2033bebb5b15686e9b437f6e
Ezra Szanton <ezraszanton@yahoo.com>
2020-01-14 11:29:14 -0500

    Initialize and create triangle mesh

---
+++ procedural_landscape/Assets/mesh_generator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class mesh_generator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        create_shape();
+        update_mesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void create_shape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2
+        };
+
+    }
+
+    void update_mesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
